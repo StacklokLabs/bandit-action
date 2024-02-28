474ea46b4c0256205c1fc69b50c55523c0601b91
Luke Hinds <luke@stacklok.com>
2024-02-28 15:57:19 +0000

    Add yaml dump, and trigger action run

--- test/main.py
+++ test/main.py
@@ -1,4 +1,5 @@
 import subprocess
+import yaml
 
 # Example of a hard-coded secret, which is a security risk
 SECRET_KEY = "this_is_a_secret_key"
@@ -12,6 +13,12 @@ def insecure_subprocess_command(user_input):
     # Insecure subprocess call with shell=True, which is vulnerable to shell injection
     command = "echo " + user_input
     subprocess.call(command, shell=True)
+
+def unsafe_yaml_dump(data):
+    # Unsafe YAML dump, which is vulnerable to arbitrary code execution
+    ystr = yaml.dump({'a' : 1, 'b' : 2, 'c' : 3})
+    y = yaml.load(ystr)
+    return yaml.dump(y)
 
 def main():
     data = b"cos\nsystem\n(S'echo Hello world!'\ntR."  # Malicious pickle data
