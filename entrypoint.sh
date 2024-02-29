a90b43e4a57ebc4ad7b6e66fcd3e284f39a9ef14
Luke Hinds <luke@stacklok.com>
2024-02-29 12:45:26 +0000

    Test flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,7 +52,7 @@ fi
 [ -n "$INPUT_EXCLUDE_PATHS" ] && cmd+=" -x $INPUT_EXCLUDE_PATHS"
 [ -n "$INPUT_BASELINE" ] && cmd+=" -b $INPUT_BASELINE"
 [ -n "$INPUT_INI_PATH" ] && cmd+=" --ini $INPUT_INI_PATH"
-[ -n "$INPUT_EXIT_ZERO" ] && cmd+=" --exit-zero"
+[ "$INPUT_EXIT_ZERO" = "true" ] && cmd+=" --exit-zero"
 
 # Specify the output format as JSON and output file, we hardcode the output file to report.json
 # as this is required to format the output for the post_comment.py script
@@ -68,16 +68,16 @@ bandit_exit_code=$?
 echo "Bandit exited with code $bandit_exit_code"
 
 # Check if exit_zero is set to "true"
-# if [ "$INPUT_EXIT_ZERO" = "true" ]; then
-#     echo "exit_zero is set to true. Exiting with code 0 regardless of Bandit findings."
-#     exit 0
-# else
-#     # If Bandit exited with a non-zero exit code and exit_zero is not true, exit this script with the Bandit exit code
-#     if [ $bandit_exit_code -ne 0 ]; then
-#         echo "Bandit found issues and exit_zero is not set to true. Exiting with code $bandit_exit_code."
-#         exit $bandit_exit_code
-#     fi
-# fi
+if [ "$INPUT_EXIT_ZERO" = "true" ]; then
+    echo "exit_zero is set to true. Exiting with code 0 regardless of Bandit findings."
+    exit 0
+else
+    # If Bandit exited with a non-zero exit code and exit_zero is not true, exit this script with the Bandit exit code
+    if [ $bandit_exit_code -ne 0 ]; then
+        echo "Bandit found issues and exit_zero is not set to true. Exiting with code $bandit_exit_code."
+        exit $bandit_exit_code
+    fi
+fi
 
 # Call post_comment.py to post the Bandit report as a comment on the pull request
 GITHUB_TOKEN=$GITHUB_TOKEN GITHUB_REPOSITORY=$GITHUB_REPOSITORY python /post_comment.py
