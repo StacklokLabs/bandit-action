96552d41c731d0fd64c7b67ff2721baffddf3283
Luke Hinds <luke@stacklok.com>
2024-03-01 09:15:23 +0000

    Rework args

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,52 +7,54 @@ github_repository=$INPUT_GITHUB_REPOSITORY
 
 # Initialize the Bandit command
 cmd="bandit"
-# # Check if the recursive flag is set
-# if [ -n "${INPUT_RECURSIVE}" ]; then
-#     cmd+=" -r"
-# fi
 
-# Check for the path input and add it to the command
-# if [ -n "${INPUT_PATH}" ]; then
-#     cmd+=" -r ${INPUT_PATH}"
-# fi
+# Initialize command variable
+cmd=""
 
-# Check for the level input and set the severity level
+# Ensure INPUT_PATH is set, default to current directory if not
+INPUT_PATH=${INPUT_PATH:-.}
+
+# Set severity level
 if [ -n "${INPUT_LEVEL}" ]; then
     case "${INPUT_LEVEL}" in
         "low") cmd+=" -l" ;;
-        "medium") cmd+=" -ll" ;;
-        "high") cmd+=" -lll" ;;
+        "medium") cmd+=" -m" ;;  # Changed from -ll to -m for medium
+        "high") cmd+=" -h" ;;    # Changed from -lll to -h for high
     esac
 fi
 
-# Check for the confidence input and set the confidence level
+# Set confidence level
 if [ -n "${INPUT_CONFIDENCE}" ]; then
     case "${INPUT_CONFIDENCE}" in
         "low") cmd+=" -i" ;;
-        "medium") cmd+=" -ii" ;;
-        "high") cmd+=" -iii" ;;
+        "medium") cmd+=" -j" ;;  # Assuming -j for medium confidence
+        "high") cmd+=" -k" ;;    # Assuming -k for high confidence
     esac
 fi
 
-
+# Flags without parameters
 [ "$INPUT_VERBOSE" = "true" ] && cmd+=" -v"
 [ "$INPUT_DEBUG" = "true" ] && cmd+=" -d"
 [ "$INPUT_QUIET" = "true" ] && cmd+=" -q"
-[ "$INPUT_IGNORE_NOSEC" = "true" ] && cmd+=" --ignore-nosec"
-[ "$INPUT_RECURSIVE" = "true" ] && cmd+=" -r"
-[ -n "$INPUT_PATH" ] && cmd+=" -r $INPUT_PATH"
+[ "$INPUT_IGNORE_NOSEC" = "true" ] && cmd+=" ---ignore-nosec"
+[ "$INPUT_EXIT_ZERO" = "true" ] && cmd+=" --exit-zero"
+
+# Set INPUT_RECURSIVE with INPUT_PATH. We hardcode -r as it is required for 
+# Bandit to run
+[ "$INPUT_RECURSIVE" = "true" ] &&
+cmd+=" -r $INPUT_PATH"
+
+# Other flags with parameters
 [ -n "$INPUT_AGGREGATE" ] && cmd+=" -a $INPUT_AGGREGATE"
 [ -n "$INPUT_CONTEXT_LINES" ] && cmd+=" -n $INPUT_CONTEXT_LINES"
 [ -n "$INPUT_CONFIG_FILE" ] && cmd+=" -c $INPUT_CONFIG_FILE"
 [ -n "$INPUT_PROFILE" ] && cmd+=" -p $INPUT_PROFILE"
 [ -n "$INPUT_TESTS" ] && cmd+=" -t $INPUT_TESTS"
 [ -n "$INPUT_SKIPS" ] && cmd+=" -s $INPUT_SKIPS"
-[ -n "$INPUT_SEVERITY_LEVEL" ] && cmd+=" --severity-level $INPUT_SEVERITY_LEVEL"
+[ -n "$INPUT_SEVERITY_LEVEL" ] && cmd+=" -s $INPUT_SEVERITY_LEVEL"
 [ -n "$INPUT_EXCLUDE_PATHS" ] && cmd+=" -x $INPUT_EXCLUDE_PATHS"
 [ -n "$INPUT_BASELINE" ] && cmd+=" -b $INPUT_BASELINE"
-[ -n "$INPUT_INI_PATH" ] && cmd+=" --ini $INPUT_INI_PATH"
-[ "$INPUT_EXIT_ZERO" = "true" ] && cmd+=" --exit-zero"
+[ -n "$INPUT_INI_PATH" ] && cmd+=" -ini $INPUT_INI_PATH"
 
 # Force the output format as JSON and output file, we json and to report.json
 # as this is required to format the output for the post_comment.py script
